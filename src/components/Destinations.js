4bf273cccb040e21101088677d69b10981df25a8
danmainah <danielmainawaweru@gmail.com>
2021-11-12 22:03:57 +0300

    Add header text for ndestination component

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -1,9 +1,10 @@
-const Destinations = ()  =>  {
-    return (
-        <>  
-           
-        </>
-    );
-}
+const Destinations = () => (
+  <div className="container">
+    <div className="align-middle">
+      <h4 className="d-flex justify-content-center">Latest Destinations</h4>
+      <h5 className="d-flex justify-content-center"> Please select Favourite Destination </h5>
+    </div>
+  </div>
+);
 
 export default Destinations;
