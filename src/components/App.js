dab6ce97e3a30ac0e6ac854a00bf12c5db9c9eb2
danmainah <danielmainawaweru@gmail.com>
2021-11-12 12:55:13 +0300

    Update the app component and deleted unused resources and components

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import '../App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <h1>Demo the app component is working</h1>
+  </Router>
+);
 
 export default App;
