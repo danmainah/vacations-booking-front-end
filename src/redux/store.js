56e775a134683e5325c7c04b4c5a07299d9eb7a5
danmainah <danielmainawaweru@gmail.com>
2021-11-12 22:41:05 +0300

    Add destination reducer and action

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-
+import { destinationsReducer } from './Destinations/destinations';
 
 const reducer = combineReducers({
-  destinations: destinationReducer,
+  destinations: destinationsReducer,
 });
 
 const store = createStore(reducer, applyMiddleware(logger, thunk));
