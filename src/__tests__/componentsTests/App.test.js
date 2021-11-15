f60e7a8834dc3f88f5dc12c931a6b93926bc15aa
Aleksandar <a.ivezic@yahoo.com>
2021-11-15 15:33:00 +0100

    Fix eslint errors

--- src/__tests__/componentsTests/App.test.js
+++ src/__tests__/componentsTests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import App from '../../components/App'
+import App from '../../components/App';
 
 describe('App', () => {
   it('renders correctly', () => {
