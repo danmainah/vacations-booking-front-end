120cf671a08706396d249e17ca3837966cd4f3f8
danmainah <danielmainawaweru@gmail.com>
2021-11-15 17:36:17 +0300

    Pass slider component into destinations component

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -1,8 +1,11 @@
+import Slider from './Slider';
+
 const Destinations = () => (
   <div className="container">
     <div className="align-middle">
       <h4 className="d-flex justify-content-center">Latest Destinations</h4>
       <h5 className="d-flex justify-content-center"> Please select Favourite Destination </h5>
+      <Slider />
     </div>
   </div>
 );
