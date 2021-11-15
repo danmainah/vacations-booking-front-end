908922202a718a7d19ae40a5f3d7c5640871a107
Hubert MUGABO <mugberto@gmail.com>
2021-11-15 16:37:40 +0200

    Style DetailList

--- src/components/destinationDetailsComponents/DetailList.js
+++ src/components/destinationDetailsComponents/DetailList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const DetailList = ({name , location, price_per_day}) => (
-  <ul>
+  <ul className="detail-list">
     <li>{name}</li>
     <li>{location}</li>
     <li>Cost: ${price_per_day}/day</li>
