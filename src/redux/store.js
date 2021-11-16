097e5925e0f8e7128fdbace90327e9b64b99f51c
Igor Koloski <koloski_igor@hotmail.com>
2021-11-16 14:23:18 +0100

    Add reservation reducer to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { destinationsReducer } from './Destinations/destinations';
+import { reservationReducer } from './Reservations/reservation';
 
 const reducer = combineReducers({
   destinations: destinationsReducer,
+  reservations: reservationReducer,
 });
 
 const store = createStore(reducer, applyMiddleware(logger, thunk));
