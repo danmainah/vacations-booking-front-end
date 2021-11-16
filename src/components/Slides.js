3daed3a71800e60e1f2e6705d875e2f1ba8ce40a
danmainah <danielmainawaweru@gmail.com>
2021-11-16 11:20:51 +0300

    Add slide component and add react-bootstrap carrosel into it.

---
+++ src/components/Slides.js
@@ -0,0 +1,118 @@
+import Carousel from 'react-bootstrap/Carousel';
+import { CardGroup, Card } from 'react-bootstrap';
+import '../styles/Slider.css';
+
+const Slides = () => (
+  <div className="container">
+    <div className="row">
+      <Carousel>
+        <Carousel.Item interval={1500}>
+          <CardGroup>
+            <Card>
+              <Card.Img variant="top" src="./one.jpg" alt="Card  cap" />
+              <Card.Body>
+                <Card.Title>Desert Experience</Card.Title>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <div className="d-flex justify-content-center">
+                  <i className="fa fa-facebook-official" />
+                  <i className="fa fa-twitter" />
+                  <i className="fa fa-linkedin-square" />
+                </div>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src="./two.jpg" alt="TWO" />
+              <Card.Body>
+                <Card.Title>Beach</Card.Title>
+                <Card.Text>
+                  In vulputate nulla sed dolor ultrices, a tincidunt lacus tincidunt.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <div className="d-flex justify-content-center">
+                  <i className="fa fa-facebook-official" />
+                  <i className="fa fa-twitter" />
+                  <i className="fa fa-linkedin-square" />
+                </div>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src="./three.jpg" alt="Three" />
+              <Card.Body>
+                <Card.Title>Forest</Card.Title>
+                <Card.Text>
+                  In vulputate nulla sed dolor ultrices, a tincidunt lacus tincidunt.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <div className="d-flex justify-content-center">
+                  <i className="fa fa-facebook-official" />
+                  <i className="fa fa-twitter" />
+                  <i className="fa fa-linkedin-square" />
+                </div>
+              </Card.Footer>
+            </Card>
+          </CardGroup>
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <CardGroup>
+            <Card>
+              <Card.Img variant="top" src="./one.jpg" alt="Card  cap" />
+              <Card.Body>
+                <Card.Title>Desert Experience</Card.Title>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <div className="d-flex justify-content-center">
+                  <i className="fa fa-facebook-official" />
+                  <i className="fa fa-twitter" />
+                  <i className="fa fa-linkedin-square" />
+                </div>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src="./two.jpg" alt="TWO" />
+              <Card.Body>
+                <Card.Title>Beach</Card.Title>
+                <Card.Text>
+                  In vulputate nulla sed dolor ultrices, a tincidunt lacus tincidunt.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <div className="d-flex justify-content-center">
+                  <i className="fa fa-facebook-official" />
+                  <i className="fa fa-twitter" />
+                  <i className="fa fa-linkedin-square" />
+                </div>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src="./three.jpg" alt="Three" />
+              <Card.Body>
+                <Card.Title>Forest</Card.Title>
+                <Card.Text>
+                  In vulputate nulla sed dolor ultrices, a tincidunt lacus tincidunt.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <div className="d-flex justify-content-center">
+                  <i className="fa fa-facebook-official" />
+                  <i className="fa fa-twitter" />
+                  <i className="fa fa-linkedin-square" />
+                </div>
+              </Card.Footer>
+            </Card>
+          </CardGroup>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  </div>
+);
+
+export default Slides;
