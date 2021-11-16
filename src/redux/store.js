691ab399310aa287eb693c7e3c1e4d3e19fa2cba
Hubert MUGABO <mugberto@gmail.com>
2021-11-16 13:02:42 +0200

    Add reservation reducer to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { destinationsReducer } from './Destinations/destinations';
+import reservationReducer from './reservations/reservationReducer';
 
 const reducer = combineReducers({
   destinations: destinationsReducer,
+  reservations: reservationReducer,
 });
 
 const store = createStore(reducer, applyMiddleware(logger, thunk));
