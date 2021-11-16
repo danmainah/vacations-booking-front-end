7fe4abe14b749ce8e630b254fe35b9ab6ea00a2f
danmainah <danielmainawaweru@gmail.com>
2021-11-16 21:51:48 +0300

    Fix linter errors

--- src/__tests__/componentsTests/Destinations.test.js
+++ src/__tests__/componentsTests/Destinations.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { create } from 'react-test-renderer';
+import renderer, { create } from 'react-test-renderer';
+
 import Destinations from '../../components/Destinations';
 
 describe('Destinations', () => {
@@ -10,21 +10,19 @@ describe('Destinations', () => {
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
-  test("the className of the component includes container", () => {
-    const root = create(<Destinations />).root;
-    const element = root.findByType("div");
-    expect(element.props.className.includes("container")).toBe(true);
+  test('the className of the component includes container', () => {
+    const { root } = create(<Destinations />);
+    const element = root.findByType('div');
+    expect(element.props.className.includes('container')).toBe(true);
   });
-  test("the  component to have h4 element", () => {
-    const root = create(<Destinations />).root;
+  test('the  component to have h4 element', () => {
+    const { root } = create(<Destinations />);
     const element = root.findByType('h4');
     expect(element.type).toBe('h4');
   });
-  test("the  component to have h5 element with right content", () => {
-    const root = create(<Destinations />).root;
+  test('the  component to have h5 element with right content', () => {
+    const { root } = create(<Destinations />);
     const element = root.findByType('h5');
-    expect(element.children.includes(" Please select Favourite Destination ")).toBe(true);
+    expect(element.children.includes(' Please select Favourite Destination ')).toBe(true);
   });
 });
-
-
