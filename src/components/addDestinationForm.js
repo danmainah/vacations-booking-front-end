31de80d3d5b12553bfdc2afd991b321038f81d0d
Igor Koloski <koloski_igor@hotmail.com>
2021-11-18 22:03:45 +0100

    Improve useState argument

--- src/components/addDestinationForm.js
+++ src/components/addDestinationForm.js
@@ -2,35 +2,35 @@ import React, { useState } from 'react';
 import postDestination from '../helpers/postDestinationData';
 
 const DestForm = () => {
-  const [name, setName] = useState('');
-  const [location, setLocation] = useState('');
-  const [imageUrl, setImageUrl] = useState('');
-  const [pricePerDay, setPricePerDay] = useState('0');
+  const [inputs, setInputs] = useState({
+    name: '', location: '', image_url: '', price_per_day: '',
+  });
+
   return (
     <form onSubmit={(e) => {
       e.preventDefault();
-      postDestination(name, location, imageUrl, pricePerDay);
+      postDestination(inputs);
       e.target.reset();
     }}
     >
       <label htmlFor="name">
         Destination Name
-        <input type="text" name="name" id="name" onChange={(e) => setName(e.target.value)} />
+        <input type="text" name="name" id="name" onChange={(e) => setInputs({ ...inputs, name: e.target.value })} />
       </label>
 
       <label htmlFor="location">
         Location
-        <input type="text" name="location" id="location" onChange={(e) => setLocation(e.target.value)} />
+        <input type="text" name="location" id="location" onChange={(e) => setInputs({ ...inputs, location: e.target.value })} />
       </label>
 
       <label htmlFor="image_url">
         Image url
-        <input type="text" name="image_url" id="image_url" onChange={(e) => setImageUrl(e.target.value)} />
+        <input type="text" name="image_url" id="image_url" onChange={(e) => setInputs({ ...inputs, image_url: e.target.value })} />
       </label>
 
       <label htmlFor="price_per_day">
         Price per day
-        <input type="number" name="price_per_day" id="price_per_day" min="0" onChange={(e) => setPricePerDay(e.target.value)} />
+        <input type="number" name="price_per_day" id="price_per_day" min="0" defaultValue="0" onChange={(e) => setInputs({ ...inputs, price_per_day: e.target.value })} />
       </label>
 
       <button type="submit">Submit</button>
--- src/helpers/postDestinationData.js
+++ src/helpers/postDestinationData.js
@@ -1,15 +1,10 @@
 const destinationUrl = 'http://localhost:3000/api/v1/destinations';
 const token = localStorage.getItem('token');
 
-const postDestination = (name, location, imageUrl, pricePerDay) => {
+const postDestination = (body) => {
   fetch(destinationUrl, {
     method: 'POST',
-    body: JSON.stringify({
-      name,
-      location,
-      image_url: imageUrl,
-      price_per_day: pricePerDay,
-    }),
+    body: JSON.stringify(body),
     headers: new Headers({
       Authorization: token,
       'Content-Type': 'application/json',
