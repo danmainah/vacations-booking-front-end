c2eea94dd90acf06fea16d5c2b77b2e3a8b4dfc8
danmainah <danielmainawaweru@gmail.com>
2021-11-18 13:22:34 +0300

    Add responsive design

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -1,11 +1,36 @@
-import Slides from './Slides';
+import Phone from './slides/Phone';
+import Tablet from './slides/Tablet';
+import Large from './slides/Large';
+
+const DesData = [
+  {
+    image:
+      './one.jpg',
+    title: 'Beach',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    image:
+      './two.jpg',
+    title: 'Forest',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    image:
+      './three.jpg',
+    title: 'Desert',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+];
 
 const Destinations = () => (
   <div className="container">
     <div className="align-middle position-absolute mt-5 pt-5">
       <h4 className="d-flex justify-content-center">Latest Destinations</h4>
       <h5 className="d-flex justify-content-center"> Please select Favourite Destination </h5>
-      <Slides />
+      <Phone />
+      <Tablet data={DesData} />
+      <Large />
     </div>
   </div>
 );
---
+++ src/components/slides/Tablet.js
@@ -0,0 +1,122 @@
+import Carousel from 'react-bootstrap/Carousel';
+import { CardGroup, Card } from 'react-bootstrap';
+import '../../styles/Slides.css';
+
+const DesData = [
+  {
+    image:
+      './one.jpg',
+    title: 'Beach',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    image:
+      './two.jpg',
+    title: 'Forest',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    image:
+      './three.jpg',
+    title: 'Desert',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+];
+
+const Tablet = () => (
+  <div className="container">
+    <div className="row d-none d-md-block d-lg-none">
+      <Carousel variant="dark">
+        <Carousel.Item interval={5000}>
+          <CardGroup className="">
+            {DesData.slice(0, 2).map((data) => (
+              <Card key={data.title}>
+                <Card.Img className="rounded-circle phone" variant="top" src={data.image} alt="Card  cap" />
+                <Card.Body>
+                  <Card.Title>
+                    <h4 className="d-flex justify-content-center ">
+                      {data.title}
+                    </h4>
+                  </Card.Title>
+                  <Card.Text>
+                    <small>
+                      {data.text}
+                    </small>
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <div className="d-flex justify-content-center">
+                    <i className="fa fa-facebook-official fa-lg" />
+                    <i className="fa fa-twitter fa-lg" />
+                    <i className="fa fa-linkedin-square fa-lg" />
+                  </div>
+                </Card.Footer>
+              </Card>
+            ))}
+            ;
+          </CardGroup>
+        </Carousel.Item>
+        <Carousel.Item interval={5000}>
+          <CardGroup>
+            {DesData.slice(1, 3).map((data) => (
+              <Card key={data.title}>
+                <Card.Img className="rounded-circle" variant="top" src={data.image} alt="Card  cap" />
+                <Card.Body>
+                  <Card.Title>
+                    <h4 className="d-flex justify-content-center ">
+                      {data.title}
+                    </h4>
+                  </Card.Title>
+                  <Card.Text>
+                    <small>
+                      {data.text}
+                    </small>
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <div className="d-flex justify-content-center">
+                    <i className="fa fa-facebook-official fa-lg" />
+                    <i className="fa fa-twitter fa-lg" />
+                    <i className="fa fa-linkedin-square fa-lg" />
+                  </div>
+                </Card.Footer>
+              </Card>
+            ))}
+            ;
+          </CardGroup>
+        </Carousel.Item>
+        <Carousel.Item interval={5000}>
+          <CardGroup>
+            {DesData.slice(2, 3).map((data) => (
+              <Card key={data.title}>
+                <Card.Img className="rounded-circle" variant="top" src={data.image} alt="Card  cap" />
+                <Card.Body>
+                  <Card.Title>
+                    <h4 className="d-flex justify-content-center ">
+                      {data.title}
+                    </h4>
+                  </Card.Title>
+                  <Card.Text>
+                    <small>
+                      {data.text}
+                    </small>
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <div className="d-flex justify-content-center">
+                    <i className="fa fa-facebook-official fa-lg" />
+                    <i className="fa fa-twitter fa-lg" />
+                    <i className="fa fa-linkedin-square fa-lg" />
+                  </div>
+                </Card.Footer>
+              </Card>
+            ))}
+            ;
+          </CardGroup>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  </div>
+);
+
+export default Tablet;
