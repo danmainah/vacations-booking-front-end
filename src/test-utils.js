bc4aef9ed143a4dbe74cf924f181240cb1054316
Hubert MUGABO <mugberto@gmail.com>
2021-11-18 12:15:07 +0200

    Mock the redux store

---
+++ src/test-utils.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render as rtlRender } from '@testing-library/react';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import reservationReducer from './redux/reservations/reservationReducer';
+
+function render(
+  ui,
+  {
+    preloadedState,
+    store = configureStore({
+      reducer: {
+        reservations: reservationReducer,
+      },
+      preloadedState,
+    }),
+    ...renderOptions
+  } = {},
+) {
+  // eslint-disable-next-line react/prop-types
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+export * from '@testing-library/react';
+export { render };
