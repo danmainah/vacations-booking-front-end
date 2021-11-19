e16c19652661668341b6ad24cf5dcac31043b5b3
Aleksandar <a.ivezic@yahoo.com>
2021-11-19 11:15:17 +0100

    Remove commented out code

--- src/components/DeleteDestinations.js
+++ src/components/DeleteDestinations.js
@@ -4,30 +4,6 @@ import Button from 'react-bootstrap/Button';
 import { deleteThunkDestination } from '../redux/Destinations/destinations';
 
 const DeleteDestinations = () => {
-  // const hardcodedDestinations = [
-  //   {
-  //     id: 1,
-  //     name: 'Bora Bora',
-  //     location: 'French Polynesia',
-  //     image_url: 'https://res.cloudinary.com/dqdrsilxz/image/upload/v1636638534/bora-bora-island_tcyoev.jpg',
-  //     price_per_day: 150,
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'Bora Bora',
-  //     location: 'French Polynesia',
-  //     image_url: 'https://res.cloudinary.com/dqdrsilxz/image/upload/v1636638534/bora-bora-island_tcyoev.jpg',
-  //     price_per_day: 150,
-  //   },
-  //   {
-  //     id: 3,
-  //     name: 'Bora Bora',
-  //     location: 'French Polynesia',
-  //     image_url: 'https://res.cloudinary.com/dqdrsilxz/image/upload/v1636638534/bora-bora-island_tcyoev.jpg',
-  //     price_per_day: 150,
-  //   },
-  // ];
-
   const destinations = useSelector((state) => state.destinations.destinations);
   const dispatch = useDispatch();
   const handleDelete = (destinationId) => {
