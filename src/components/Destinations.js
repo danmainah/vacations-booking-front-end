35e95181c03df49ec3fe1758c3f3804d739ca6b8
Daniel Maina <danielmainawaweru@gmail.com>
2021-11-19 15:40:12 +0300

    Revert "Style destinations"

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -1,78 +1,24 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import Carousel from 'react-bootstrap/Carousel';
-import { CardGroup } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 import Slides from './Slides';
-import { loadDestinationsThunk } from '../redux/Destinations/destinations';
 
 const Destinations = () => {
-  const dispatch = useDispatch();
+  let destinations;
 
-  useEffect(() => {
-    dispatch(loadDestinationsThunk());
-  }, [dispatch]);
+  const state = useSelector((state) => state.destinations.destination);
 
-  const data = useSelector((state) => state.destinations.destination);
-  const state = data.sort(() => 0.5 - Math.random());
+  if (state) {
+    destinations = state.map((dest) => (
+      <h1 key={dest.image_url}>{dest.location}</h1>
+    ));
+  }
+
   return (
     <div className="container">
       <div className="align-middle">
         <h4 className="d-flex justify-content-center">Latest Destinations</h4>
         <h5 className="d-flex justify-content-center"> Please select Favourite Destination </h5>
-        <div className="row d-none d-lg-block">
-          <Carousel variant="dark">
-            <Carousel.Item interval={5000}>
-              <CardGroup className="">
-                {state.slice(0, 3).map((data) => (
-                  <Slides key={data.id} info={data} />
-                ))}
-              </CardGroup>
-            </Carousel.Item>
-            <Carousel.Item interval={5000}>
-              <CardGroup className="">
-                {state.slice(0, 3).map((data) => (
-                  <Slides key={data.id} info={data} />
-                ))}
-              </CardGroup>
-            </Carousel.Item>
-          </Carousel>
-        </div>
-        <div className="row d-none d-md-block d-lg-none">
-          <Carousel variant="dark">
-            <Carousel.Item interval={5000}>
-              <CardGroup className="">
-                {state.slice(0, 2).map((data) => (
-                  <Slides key={data.id} info={data} />
-                ))}
-              </CardGroup>
-            </Carousel.Item>
-            <Carousel.Item interval={5000}>
-              <CardGroup className="">
-                {state.slice(0, 2).map((data) => (
-                  <Slides key={data.id} info={data} />
-                ))}
-              </CardGroup>
-            </Carousel.Item>
-          </Carousel>
-        </div>
-        <div className="row d-md-none">
-          <Carousel variant="dark">
-            <Carousel.Item interval={5000}>
-              <CardGroup className="">
-                {state.slice(0, 1).map((data) => (
-                  <Slides key={data.id} info={data} />
-                ))}
-              </CardGroup>
-            </Carousel.Item>
-            <Carousel.Item interval={5000}>
-              <CardGroup className="">
-                {state.slice(0, 1).map((data) => (
-                  <Slides key={data.id} info={data} />
-                ))}
-              </CardGroup>
-            </Carousel.Item>
-          </Carousel>
-        </div>
+        <Slides />
+        {destinations}
       </div>
     </div>
   );
