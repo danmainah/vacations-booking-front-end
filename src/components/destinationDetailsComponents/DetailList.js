307f2b431e9413bb8990b4f7a3db2fb2d90e97c0
Igor Koloski <koloski_igor@hotmail.com>
2021-11-23 18:57:41 +0100

    Style destination details

--- src/components/destinationDetailsComponents/DetailList.js
+++ src/components/destinationDetailsComponents/DetailList.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './DetailList.css';
 
 const DetailList = ({ name, location, pricePerDay }) => (
-  <ul className="detail-list">
-    <li>{name}</li>
-    <li>{location}</li>
-    <li>
-      Cost: $
-      {pricePerDay}
-      /day
+  <ul className="detail-list my-5">
+    <li className="d-flex justify-content-between px-2 py-1">
+      <span>
+        Name:
+      </span>
+      <span>
+        {name}
+      </span>
+    </li>
+    <li className="d-flex justify-content-between px-2 py-1">
+      <span>
+        Location:
+      </span>
+      <span>
+        {location}
+      </span>
+
+    </li>
+    <li className="d-flex justify-content-between px-2 py-1">
+      <span>
+        Cost:
+      </span>
+      <span>
+        {pricePerDay}
+        $/day
+      </span>
     </li>
   </ul>
 );
