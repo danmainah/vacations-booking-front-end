7ab57abcd51be67d73547a130f3bf52388ecbf28
themonster2015 <quynhyen.vothi@gmail.com>
2021-11-25 09:10:40 +0100

    refactored menuitems

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import { logOutUser } from '../redux/Auth/auth';
 import NavItem from './NavItem';
+import styles from '../styles/Navbar.module.css';
 
 export default function MenuItems() {
   const loggedIn = useSelector((state) => state.user.logged_in);
   const isAdmin = useSelector((state) => state.user.admin);
   const dispatch = useDispatch();
+
+  const activeStyle = {
+    color: 'white',
+    backgroundColor: '#97BF11',
+  };
 
   const handleLogOut = (e) => {
     e.preventDefault();
@@ -52,115 +59,62 @@ export default function MenuItems() {
       clickHandler: null,
 
     },
-    {
-      to: '/',
-      text: 'LOG OUT',
-      admin: false,
-      protected: true,
-      clickHandler: handleLogOut,
-    },
-    {
-      to: '/login',
-      text: 'LOG IN',
-      admin: false,
-      protected: false,
-      clickHandler: null,
-    },
   ];
 
   return (
-
-    urls.map((url) => {
-      if (url.admin) {
-        return (isAdmin && (
+    <>
+      { urls.map((url) => {
+        if (url.admin) {
+          return (isAdmin && (
           <NavItem
             key={Math.floor(Math.random() * (1000 - 1) + 1)}
             to={url.to}
             text={url.text}
             clickHandler={url.clickHandler}
           />
-        ));
-      } if (url.protected) {
-        return (loggedIn && (
+          ));
+        } if (url.protected) {
+          return (loggedIn && (
           <NavItem
             key={Math.floor(Math.random() * (1000 - 1) + 1)}
             to={url.to}
             text={url.text}
             clickHandler={url.clickHandler}
           />
-        ));
-      }
-      return (
-        <NavItem
-          key={Math.floor(Math.random() * (1000 - 1) + 1)}
-          to={url.to}
-          text={url.text}
-          clickHandler={url.clickHandler}
-        />
-      );
-    }) // <>
-    //   <NavLink
-    //     to="/"
-    //     style={({ isActive }) => (isActive ? activeStyle : undefined)}
-    //   >
-    //     DESTINATIONS
+          ));
+        }
+        return (
+          <NavItem
+            key={Math.floor(Math.random() * (1000 - 1) + 1)}
+            to={url.to}
+            text={url.text}
+            clickHandler={url.clickHandler}
+          />
+        );
+      })}
 
-  //   </NavLink>
-  //   <NavLink
-  //     to="/reserve"
-  //     style={({ isActive }) => (isActive ? activeStyle : undefined)}
-  //   >
-  //     MAKE RESERVATIONS
+      {loggedIn ? (
 
-  //   </NavLink>
-  //   {loggedIn && (
-  //   <NavLink
-  //     to="/reservations"
-  //     style={({ isActive }) => (isActive ? activeStyle : undefined)}
-  //   >
-  //     MY RESERVATIONS
+        <NavLink
+          to="/"
+          className={styles.buttonStyle}
+          onClick={handleLogOut}
+        >
+          LOG OUT
 
-  //   </NavLink>
-  //   )}
-  //   {admin && (
-  //   <NavLink
-  //     to="/dest"
-  //     style={({ isActive }) => (isActive ? activeStyle : undefined)}
-  //   >
-  //     ADD DESTINATION
+        </NavLink>
 
-  //   </NavLink>
-  //   )}
-  //   {admin && (
-  //   <NavLink
-  //     to="/destinations/delete"
-  //     style={({ isActive }) => (isActive ? activeStyle : undefined)}
-  //   >
-  //     DELETE DESTINATION
+      )
+        : (
+          <NavLink
+            to="/login"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            LOG IN
 
-  //   </NavLink>
-  //   )}
-  //   {loggedIn ? (
+          </NavLink>
+        )}
 
-  //     <NavLink
-  //       to="/"
-  //       className={styles.buttonStyle}
-  //       onClick={handleLogOut}
-  //     >
-  //       LOG OUT
-
-  //     </NavLink>
-
-  //   )
-  //     : (
-  //       <NavLink
-  //         to="/login"
-  //         style={({ isActive }) => (isActive ? activeStyle : undefined)}
-  //       >
-  //         LOG IN
-
-  //       </NavLink>
-  //     )}
-  // </>
+    </>
   );
 }
